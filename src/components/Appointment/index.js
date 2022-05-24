ae1135985a8e94035e33029bd8dccfa5a2cafb3f
serra <serra@modemgirl.ca>
2022-05-24 15:53:28 -0700

    add form component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import "components/Appointment/styles.scss";
 
-
+import { Header, Show, Empty } from '.';
 
 export default function Appointment(props){
 
 
-return (
+  return (
 
-<article className="appointment"></article>
+  <article className="appointment"></article>
 
-)}
+  )
+}
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -4,25 +4,15 @@ import classNames from "classnames";
 import 'components/InterviewerListItem.scss';
 
 
-
-
 export default function InterviewerListItem(props) {
 
   let interviewerClass = classNames( 'interviewers__item', {' interviewers__item--selected': props.selected })
 
   return(
-
     <li className={interviewerClass} onClick={props.onChange} >
-    
-  <img
-    className="interviewers__item-image"
-    src={props.avatar}
-    alt={props.name}
-  />
-
-  {props.selected && props.name}
-  </li>
-
+      <img className="interviewers__item-image" src={props.avatar} alt={props.name} />
+      {props.selected && props.name}
+    </li>
   )}
 
 
