e00a8ace74056a1755c8e7aab7e139b2dd55cc41
serra <serra@modemgirl.ca>
2022-06-13 16:27:54 -0700

    add tests for edit and cancel interview

--- cypress/e2e/appointments.cy.js
+++ cypress/e2e/appointments.cy.js
@@ -1,10 +1,12 @@
 
 describe("Appointment", () => {
 
-  it("should book an interview", () => {
+  beforeEach(() => {
+    cy.request('GET', 'api/debug/reset')  //reset db
+    cy.visit('/').contains("Monday");     //visit root url 
+  })
 
-    // Visits the root of our web server
-    cy.visit("/").contains("Monday");
+  it("should book an interview", () => {
 
     // Clicks on the "Add" button in the second appointment
     cy.get('[alt="Add"]').first().click()
@@ -12,39 +14,44 @@ describe("Appointment", () => {
     //find the input    
     // Enters their name
     cy.get('[data-testid="student-name-input"]').type("Fjord Windstar")
-    // <input class="appointment__create-input text--semi-bold" name="name" type="text" placeholder="Enter Student Name" data-testid="student-name-input" value="">
     
     // Chooses an interviewer
-    // <img class="interviewers__item-image" src="https://i.imgur.com/LpaY82x.png" alt="Sylvia Palmer">
     cy.get('[alt="Sylvia Palmer"]').click()
     
     // Clicks the save button
-    cy.contains('[data-cy="submit"]', 'Save').click()
+    cy.contains('Save').click()
 
     // Sees the booked appointment
-    cy.visit("/").contains("Fjord Windstar")
-
+    cy.contains(".appointment__card--show", "Fjord Windstar");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
 
   })
 
-  // it("should edit an interview", () => {
+  it("should edit an interview", () => {
     
-  //   // Visits the root of our web server
-  //   // Clicks the edit button for the existing appointment
-  //   // Changes the name and interviewer
-  //   // Clicks the save button
-  //   // Sees the edit to the appointment
+    cy.get('[alt="Edit"]').first().click({force:true})  //force the click to override 'waiting for actionability' 
+    cy.get('[data-testid="student-name-input"]').clear().type("Glen Windstar")
+    cy.get('[alt="Tori Malcolm"]').click()
+    cy.contains('Save').click()
+    cy.contains(".appointment__card--show", "Glen Windstar");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
 
-  // })
+  })
 
 
-  // it("should cancel an interview", () => {
-    
-  //   // Visits the root of our web server
-  //   // Clicks the delete button for the existing appointment
-  //   // Clicks the confirm button
-  //   // Sees that the appointment slot is empty
+  it("should cancel an interview", () => {
 
-  // })
+    //Clicks the delete button
+    cy.get('[alt="Delete"]').first().click({force:true})
+
+    // Clicks the confirm button
+    cy.contains('[data-cy="submit"]', 'Yes').click()
+
+    // Sees that the appointment slot is empty
+    cy.contains('Deleting...').should('exist')
+    cy.contains('Deleting...').should('not.exist')
+    cy.contains('.appointment__card--show', 'Archie Cohen').should('not.exist')
+
+  })
 
 })
